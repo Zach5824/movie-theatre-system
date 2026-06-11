535a2c3c9f8270d594bb9f9f4a030e45dc469874
Zach5824 <kazokoth58@gmail.com>
2026-06-11 18:55:18 +0300

    class methods added

--- models/movie.py
+++ models/movie.py
@@ -17,4 +17,28 @@ class Movie:
 
         Movie.all_movies.append(self)
 
-   
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or not value.strip():
+            raise ValueError("Movie title cannot be empty.")
+        self._title = value.strip()
+
+    @property
+    def duration_mins(self):
+        return self._duration_mins
+
+    @duration_mins.setter
+    def duration_mins(self, value):
+        try:
+            val = int(value)
+        except (ValueError, TypeError):
+            raise ValueError("Duration must be a valid integer.")
+        if val <= 0:
+            raise ValueError("Duration must be greater than 0.")
+        self._duration_mins = val
+
+    
