7901be91e0b2441ef33353a28424fcb15f56a995
Zach5824 <kazokoth58@gmail.com>
2026-06-11 20:40:37 +0300

    screening method and setter

--- models/screening.py
+++ models/screening.py
@@ -17,4 +17,18 @@ class Screening:
 
         Screening.all_screenings.append(self)
 
+    @property
+    def hall_number(self):
+        return self._hall_number
+
+    @hall_number.setter
+    def hall_number(self, value):
+        try:
+            val = int(value)
+        except (ValueError, TypeError):
+            raise ValueError("Hall number must be an integer.")
+        if val <= 0:
+            raise ValueError("Hall number must be a positive number.")
+        self._hall_number = val
+
     
